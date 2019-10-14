26a2b0b45e3b28b9525fc8b3d2cbda760842a835
Felix <chuchu40507@gmail.com>
2019-10-13 21:53:14 -0400

    pytorch example

---
+++ data/pytorch_example/os.m
@@ -0,0 +1,12 @@
+Dataflow {
+    TemporalMap(16,16) K;
+    SpatialMap(Sz(R),1) Y;
+    TemporalMap(Sz(S),1) X;
+    TemporalMap(1,1) C;
+    Cluster(16, P);
+    SpatialMap(1,1) K;
+    TemporalMap(Sz(R),1) Y;
+    TemporalMap(Sz(S),1) X;
+    TemporalMap(Sz(R),7) R;
+    TemporalMap(Sz(S),7) S;
+}
