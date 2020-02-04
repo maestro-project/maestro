7ca29c8b0a139c578af485076b75d90f2e4a9079
Geonhwa Jeong <ghjeong12@gmail.com>
2020-02-04 18:19:18 -0500

    Change lib to cost-model and scripts accordingly

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 ./maestro --print_res=true \
           --print_res_csv_file=true \
           --print_log_file=false \
-          --DFSL_file='data/DFSL_description/Resnet50_dla.m' \
+          --DFSL_file='data/mapping/Resnet50_dla.m' \
           --noc_bw=32 \
           --noc_hops=1 \
           --noc_hop_latency=1 \
@@ -9,5 +9,5 @@
           --l2_size=2048 \
           --num_pes=256 \
           --print_design_space=true \
-          --msg_print_lv=0 
+          --msg_print_lv=0
 
