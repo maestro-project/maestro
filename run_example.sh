19d44e4b566f72b4837f2e5853a5d694612a28b8
Geonhwa Jeong <ghjeong12@gmail.com>
2020-10-15 15:59:38 -0400

    Update jupyter notebooks

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,5 @@
 ./maestro --HW_file='data/hw/accelerator_1.m' \
-          --Mapping_file='data/mapping/Resnet50_kcp_ws_2_1_2.m' \
+          --Mapping_file='data/mapping/Resnet50_kcp_ws.m' \
           --print_res=true \
           --print_res_csv_file=true \
           --print_log_file=false \
--- tools/jupyter_notebook/graph_util.py
+++ tools/jupyter_notebook/graph_util.py
@@ -13,3 +13,27 @@ def draw_graph(df, y, x, color, figsize, legend, title, xlabel, ylabel, start_la
     my_plot.set_xlabel(xlabel)
     my_plot.set_ylabel(ylabel)
     return my_plot
+
+def draw_two_graph(df1, df2, y, x, color, figsize, legend, title, xlabel, ylabel, start_layer, end_layer):
+    df1.columns = df1.columns.str.strip()
+    if(end_layer == "all"):
+        df1 = df1
+    else:
+        df1 = df1[start_layer:end_layer]
+        
+    df2.columns = df2.columns.str.strip()
+    if(end_layer == "all"):
+        df2 = df2
+    else:
+        df2 = df2[start_layer:end_layer]
+    
+    fig, axes = plt.subplots(nrows=1, ncols=1)
+
+    df1.plot(kind='Line', y = y, x = x, color = 'red', figsize = figsize, legend = legend, ax=axes)
+    df2.plot(kind='Line', y = y, x = x, color = 'blue', figsize = figsize, legend = legend, ax=axes)
+        
+    #my_plot = df_new.plot(kind='bar', y = y, x = x, color = color, figsize = figsize, legend = legend)
+    #fig.set_title(title)
+    #fig.set_xlabel(xlabel)
+    #fig.set_ylabel(ylabel)
+    return fig
