11a684bc562b01f929634638e2871c577c36feb2
Felix <felix@gatech.edu>
2021-05-08 16:35:13 -0400

    Update area, power, energy model

--- cost-model/include/design-space-exploration/DSE_cost-database.hpp
+++ cost-model/include/design-space-exploration/DSE_cost-database.hpp
@@ -23,64 +23,64 @@ Author : Hyoukjun Kwon (hyoukjun@gatech.edu)
 #ifndef MAESTRO_DSE_COST_DATABASE_HPP_
 #define MAESTRO_DSE_COST_DATABASE_HPP_
 
-namespace maestro {
-  namespace DSE {
-    namespace cost {
-      /* Embedded scaling model */
-
-      const double mac_area = 4470.9014;
-      const double mac_power = 1.2223;
-      const double mac_energy = 0.904; // nJ; Caution, it's from external source!
-
-      const double sram_unit_size_64 = 64;
-      const double sram_area_64 = 4505.1889;
-      const double sram_power_64 = 0.2208;
-
-      const double sram_unit_size_32768 = 32768;
-      const double sram_area_32768 = 4161.536;
-      const double sram_power_32768 = 4;
-
-      const double bus_unit_area = 101.7938;
-      const double bus_unit_power = 0.8;
-
-      const double arbiter_unit_area = 395.473;
-      const double arbiter_unit_power = 0.34;
-
-    }; // End of namespace scaling_model
-  }; // End of namespace DSE
-}; // End of namespace maestro
+//namespace maestro {
+//  namespace DSE {
+//    namespace cost {
+//      /* Embedded scaling model */
+//
+//      const double mac_area = 4470.9014;
+//      const double mac_power = 1.2223;
+//      const double mac_energy = 0.904; // nJ; Caution, it's from external source!
+//
+//      const double sram_unit_size_64 = 64;
+//      const double sram_area_64 = 4505.1889;
+//      const double sram_power_64 = 0.2208;
+//
+//      const double sram_unit_size_32768 = 32768;
+//      const double sram_area_32768 = 4161.536;
+//      const double sram_power_32768 = 4;
+//
+//      const double bus_unit_area = 101.7938;
+//      const double bus_unit_power = 0.8;
+//
+//      const double arbiter_unit_area = 395.473;
+//      const double arbiter_unit_power = 0.34;
+//
+//    }; // End of namespace scaling_model
+//  }; // End of namespace DSE
+//}; // End of namespace maestro
 
 
 //Felix
 // ref:"https://www.tomshardware.com/news/bfloat16-deep-dive-arm-bf16-support-armv8-a,40305.html"
 // ref:"https://cacm.acm.org/magazines/2020/7/245701-domain-specific-hardware-accelerators/fulltext"
-//namespace maestro {
-//    namespace DSE {
-//        namespace cost {
-//            /* Embedded scaling model */
+namespace maestro {
+    namespace DSE {
+        namespace cost {
+            /* Embedded scaling model */
+
+            const double mac_area = 283;
+            const double mac_power = 1.2223;
+            const double mac_energy = 0.00023; // nJ; Caution, it's from external source!
+
+            const double sram_unit_size_64 = 1;
+            const double sram_area_64 = 0.104;
+            const double sram_power_64 = 0.00345;
+
+            const double sram_unit_size_32768 = 1;
+            const double sram_area_32768 = 0.104;
+            const double sram_power_32768 = 0.00345;
+
+            const double bus_unit_area = 101.7938;
+            const double bus_unit_power = 0.8;
+
+            const double arbiter_unit_area = 395.473;
+            const double arbiter_unit_power = 0.34;
+
+        }; // End of namespace scaling_model
+    }; // End of namespace DSE
+}; // End of namespace maestro
 //
-//            const double mac_area = 283;
-//            const double mac_power = 1.2223;
-//            const double mac_energy = 0.00023; // nJ; Caution, it's from external source!
-//
-//            const double sram_unit_size_64 = 1;
-//            const double sram_area_64 = 0.104;
-//            const double sram_power_64 = 0.00345;
-//
-//            const double sram_unit_size_32768 = 1;
-//            const double sram_area_32768 = 0.104;
-//            const double sram_power_32768 = 0.00345;
-//
-//            const double bus_unit_area = 101.7938;
-//            const double bus_unit_power = 0.8;
-//
-//            const double arbiter_unit_area = 395.473;
-//            const double arbiter_unit_power = 0.34;
-//
-//        }; // End of namespace scaling_model
-//    }; // End of namespace DSE
-//}; // End of namespace maestro
-////
 
 
 
